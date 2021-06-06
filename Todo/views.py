deebe78b0714d76388728e22395c127d9f826a99
Thiago <thiagomarbsn@gmail.com>
2021-06-05 21:42:14 -0300

    finished tasks view

--- Todo/views.py
+++ Todo/views.py
@@ -1,5 +1,6 @@
-from django.http.response import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect, HttpResponseRedirectBase
+from django.urls import reverse
+from django.shortcuts import redirect, render
 from django.template import loader
 from ToDo.models import Todo
 
@@ -7,15 +8,15 @@ from ToDo.models import Todo
 def index(request):
     template = loader.get_template('ToDo/index.html')
 
-    todo_items = Todo.objects.all().order_by("added_date")
+    todo_items = Todo.objects.all().order_by("-added_date")
     context = {
         "items": todo_items
     }
-    
+
     return HttpResponse(template.render(context, request))
 
 def add_todo(request):
     content = request.POST["taskInput"]
     Todo.objects.create(text=content)
 
-    return HttpResponseRedirect('')
+    return HttpResponseRedirect("/")
