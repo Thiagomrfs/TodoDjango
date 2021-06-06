d8b42c58818f0f91dbd60191ef0b6a8e11a5470e
Thiago <thiagomarbsn@gmail.com>
2021-06-05 21:19:49 -0300

    created add_todo_item view

--- Todo/views.py
+++ Todo/views.py
@@ -1,11 +1,21 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
+from ToDo.models import Todo
 
 
 def index(request):
     template = loader.get_template('ToDo/index.html')
+
+    todo_items = Todo.objects.all().order_by("added_date")
     context = {
-        'teste': 1
+        "items": todo_items
     }
+    
     return HttpResponse(template.render(context, request))
+
+def add_todo(request):
+    content = request.POST["taskInput"]
+    Todo.objects.create(text=content)
+
+    return HttpResponseRedirect('')
